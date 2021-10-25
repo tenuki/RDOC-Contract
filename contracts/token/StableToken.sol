c4b56678fc053f64b7b40ad3ba5bd3d6d94997dc
david weil <david.weil@coinfabrik.com>
2021-10-25 12:07:10 -0300

    update for BSC-btcb rdoc-like stable coin

--- contracts/token/StableToken.sol
+++ contracts/token/StableToken.sol
@@ -5,8 +5,8 @@ import "./OwnerBurnableToken.sol";
 
 contract StableToken is ERC20Detailed, OwnerBurnableToken {
 
-  string private _name = "RIF Dollar on Chain";
-  string private _symbol = "RDOC";
+  string private _name = "BTCB Dollar on Chain";
+  string private _symbol = "DOC";
   uint8 private _decimals = 18;
 
   constructor() Ownable() ERC20Detailed(_name, _symbol, _decimals) public {
